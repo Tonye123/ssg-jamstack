519697b9091a59b1e2fa5ad5744a67c698b5ae16
tobe <triviao85@gmail.com>
2020-03-14 15:20:08 -0600

    initial commit

---
+++ src/js/vibe.js
@@ -0,0 +1,12 @@
+const vibes = [
+    "...and you are awesome",
+    "... have a wonderful day",
+    "...and you've got this",
+    "...keep rocking"
+
+
+]
+
+
+
+document.querySelector('.vibe').append(vibes[Math.floor(Math.random() * vibes.length)])
